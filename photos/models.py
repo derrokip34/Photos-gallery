63dd14b5bc5b63540a9183dcadfe31d3a223941b
derrickip34 <derrickip34@gmail.com>
2020-05-21 14:41:58 +0300

    add image_description to Image model

--- photos/models.py
+++ photos/models.py
@@ -5,6 +5,7 @@ import datetime as dt
 class Image(models.Model):
     image = models.ImageField(upload_to='images/',default='DEFAULT VALUE')
     image_name = models.CharField(max_length=60)
+    image_description = models.CharField(max_length=200,null=True)
     date_posted = models.DateTimeField(auto_now_add=True)
     image_category = models.ForeignKey('Category',on_delete=models.CASCADE,)
     image_location = models.ForeignKey('Location',on_delete=models.CASCADE,)
