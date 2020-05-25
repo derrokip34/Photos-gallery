2aee8bac966e5930647987f3f784ca25f12d8f03
derrickip34 <derrickip34@gmail.com>
2020-05-25 13:10:37 +0300

    change databases in settings.py

--- gallery/settings.py
+++ gallery/settings.py
@@ -82,17 +82,17 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME', 'photos'),
         'USER': os.environ.get('DB_USER', 'derrick'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'montolivo'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-        'TEST': {
-            'NAME': ''
-        }
+        'PORT': os.environ.get('DB_PORT', '5432')
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
